83797e4273f86727bc6fc142888ed21c7b9163a9
Nebu Mathews <deneasta@gmail.com>
2026-08-18 13:23:27 +0100

    Example removed

--- example/tf_gitops/backends/dev.hcl
+++
@@ -1,5 +0,0 @@
-key    = "tf-gitops-dev.tfstate"
-bucket = "tf-gitops-dev-terraform-state20260629105301750300000001"
-region = "eu-west-1"
-#This is to be used when run on local machine. Not to be used with gitops.
-#profile        = "ntegra-sb-sso-adm"
--- example/tf_gitops/create_backend_storage/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_info" {
-  description = "Info to use for the Terraform S3 backend."
-  value = {
-    bucket      = aws_s3_bucket.terraform_state.bucket
-    region      = var.aws_region
-    environment = var.environment
-    workspace   = terraform.workspace
-  }
-}
